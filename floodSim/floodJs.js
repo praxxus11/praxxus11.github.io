d162f77a391aedf29f3454639c4b810eb35a1ce2
Eric Huang <erichuangg99@gmail.com>
2020-09-10 12:02:10 -0700

    'ferf'

--- floodSim/floodJs.js
+++ floodSim/floodJs.js
@@ -141,7 +141,7 @@ function fillTime() {
     floodFill(startX, startY);
 }
 function rand() {
-    if (allPixel.length()>10) {
+    if (allPixel.length>10) {
         for (let i=0; i<50; i++) {
             for (let j=0; j<50; j++) {
                 if (Math.random()<0.2) {
