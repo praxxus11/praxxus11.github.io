737bcb2920f39fb2cacfde342aded2486a63e534
Eric Huang <erichuangg99@gmail.com>
2020-11-19 12:50:18 -0800

    Added way more options!!!!!

--- grav/gravJs.js
+++ grav/gravJs.js
@@ -4,10 +4,16 @@ let mouseX;
 let mouseY;
 let clicking;
 let arr = [];
+let colliding = [];
 let infoScreen = false; 
 let gravOn = false;
 let clicked = false;
 let gravClicked = false;
+let obl = false;
+let oblClicked = false;
+let bGrav = false;
+let gGravClicked = false;
+let numBalls = 77;
 
 function currPos(event) {
     mouseX = event.offsetX;
@@ -28,8 +34,49 @@ window.onload = function() {
     cvs.width = window.innerWidth-20;
     cvs.height = window.innerHeight-30;
     ctx = cvs.getContext("2d");
-    // alert("Click around the screen!");
     start();
+}
+
+function calcColl(ax, ay, bx, by, adx, ady, bdx, bdy, ma, mb) {
+    if (adx==0) adx+=1e-7;
+    if (ady==0) ady+=1e-7;
+    if (bdx==0) bdx+=1e-7;
+    if (bdy==0) bdy+=1e-7;
+    ma=ma*ma;
+    mb=mb*mb;
+    let dx = bx-ax;
+    let dy = by-ay;
+    dy*=-1;
+    let theta = Math.atan(dy/dx);
+    if (ax>=bx) theta += Math.PI;
+    ady*=-1;
+    bdy*=-1;
+    let atheta = Math.atan(ady/adx);
+    let btheta = Math.atan(bdy/bdx);
+    if (adx<0) atheta += Math.PI;
+    if (bdx<0) btheta += Math.PI;
+    let aThetaDiff = atheta-theta;
+    let bThetaDiff = btheta-theta;
+    let aVelMag = Math.sqrt(Math.pow(adx,2)+Math.pow(ady,2));
+    let bVelMag = Math.sqrt(Math.pow(bdx,2)+Math.pow(bdy,2));
+
+    let newaDx = aVelMag * Math.cos(aThetaDiff);
+    let newaDy = aVelMag * Math.sin(aThetaDiff);
+    let newbDx = bVelMag * Math.cos(bThetaDiff);
+    let newbDy = bVelMag * Math.sin(bThetaDiff);
+
+    let vap = (ma*newaDx + 2*mb*newbDx - mb*newaDx)/(ma + mb);
+    let vbp = (2*ma*newaDx + mb*newbDx - ma*newbDx)/(ma + mb);
+
+    let newAngA = Math.atan(newaDy/vap) + theta;
+    let newAngB = Math.atan(newbDy/vbp) + theta;
+    if (vap<0) newAngA += Math.PI;
+    if (vbp<0) newAngB += Math.PI;
+
+    let augMagA = Math.sqrt(Math.pow(vap,2)+Math.pow(newaDy,2));
+    let augMagB = Math.sqrt(Math.pow(vbp,2)+Math.pow(newbDy,2));
+
+    return [augMagA*Math.cos(newAngA), -augMagA*Math.sin(newAngA), augMagB*Math.cos(newAngB), -augMagB*Math.sin(newAngB)];
 }
 
 function Particle(x, y, dy, dx, rad, col) {
@@ -74,8 +121,9 @@ function Particle(x, y, dy, dx, rad, col) {
         ctx.closePath();
     };
 }
+
 function rand(low, high) {
-    return Math.random()*high+low;
+    return Math.random()*(high-low)+low;
 }
 
 function getAccels(k, m1, m2, x1, y1, x2, y2) {
@@ -97,6 +145,58 @@ function getAccels(k, m1, m2, x1, y1, x2, y2) {
     
     return [-forceX/m2, forceY/m2];
 }
+
+function interaction(i, j) {
+    if (obl && i!=j) {
+        let dist = Math.sqrt(Math.pow(arr[i].x-arr[j].x,2)+Math.pow(arr[i].y-arr[j].y,2));
+        if (dist<arr[i].rad+arr[j].rad) {
+            let e = 0.9;
+            let hi = calcColl(arr[i].x, arr[i].y, arr[j].x, arr[j].y, arr[i].dx, arr[i].dy, arr[j].dx, arr[j].dy, arr[i].rad, arr[j].rad);
+            if (!colliding[i][j] || !colliding[j][i]) {
+                arr[i].dx = e*hi[0];
+                arr[i].dy = e*hi[1];
+                arr[j].dx = e*hi[2];
+                arr[j].dy = e*hi[3];
+                colliding[i][j]=true;
+                colliding[j][i]=true;
+            }
+            else {
+                let dx = arr[i].x - arr[j].x;
+                let dy = arr[i].y - arr[j].y;
+                dy*=-1; 
+                let theta = Math.atan(dy/dx);
+                if (dx<0) theta += Math.PI;
+                let a = (arr[i].rad+arr[j].rad-dist) * Math.cos(theta);
+                let b = -(arr[i].rad+arr[j].rad-dist) * Math.sin(theta);
+                let c = (arr[i].rad+arr[j].rad-dist) * Math.cos(theta+Math.PI);
+                let d = -(arr[i].rad+arr[j].rad-dist) * Math.sin(theta+Math.PI);
+                let scale = 0.6;
+
+                withinBounds = (rad, corr, l, r) => !(corr-rad<l || corr+rad>r);
+                if (withinBounds(arr[i].rad, arr[i].x+scale*a, 0, cvs.width)) arr[i].x += scale*a;
+                if (withinBounds(arr[i].rad, arr[i].y+scale*b, 0, cvs.height)) arr[i].y += scale*b;
+                if (withinBounds(arr[j].rad, arr[j].x+scale*c, 0, cvs.width)) arr[j].x += scale*c;
+                if (withinBounds(arr[j].rad, arr[j].y+scale*d, 0, cvs.height)) arr[j].y += scale*d;
+            }
+        }
+        else {
+            colliding[i][j]=false;
+            colliding[j][i]=false;
+        }
+    }
+    if (bGrav) {
+        let grav1 = getAccels(1, Math.pow(arr[i].rad,2), Math.pow(arr[j].rad,2), arr[i].x, arr[i].y, arr[j].x, arr[j].y);
+        let grav2 = getAccels(1, Math.pow(arr[j].rad,2), Math.pow(arr[i].rad,2), arr[j].x, arr[j].y, arr[i].x, arr[i].y);
+        let fact = (obl?0.3:0.5);
+        if (!Number.isNaN(grav1[0])) {
+            arr[i].dx += fact*grav2[0];
+            arr[i].dy += fact*grav2[1];
+            arr[j].dx += fact*grav1[0];
+            arr[j].dy += fact*grav1[1];
+        }
+    }
+}
+
 function playing() {
     requestAnimationFrame(playing);
     ctx.fillStyle = 'black'
@@ -104,32 +204,48 @@ function playing() {
     ctx.font = '40px Arial';
     ctx.fillStyle = 'rgba(255,255,255,0.5)';
     ctx.fillText("ⓘ", 5, 40);
-    for (let i=0; i<150; i++) arr[i].update();
-
     if (mouseX>0 && mouseX<45 && mouseY>0 && mouseY<45 && clicking) {
         infoScreen = true;
         if (clicked) {
             infoScreen = false;
         }
     }
-    if (mouseX>160 && mouseX<185 && mouseY>75 && mouseY<100 && clicking) {
-        if (clicked) {
+    if (clicking) {
+        if (mouseX>160 && mouseX<185 && mouseY>75 && mouseY<100 && clicked) {
             gravOn = true;
             if (gravClicked) {
                 gravOn = false;
             }
         }
+        if (mouseX>190 && mouseX<190+25 && mouseY>115 && mouseY<115+25 && clicked) {
+            obl = true;
+            if (oblClicked) {
+                obl = false;
+            }
+        }
+        if (mouseX>170 && mouseX<170+25 && mouseY>155 && mouseY<155+25 && clicked) {
+            bGrav = true;
+            if (bGravClicked) {
+                bGrav = false;
+            }
+        }
     }
+    arr.forEach((ball) => ball.update());
+    for (let i=0; i<numBalls; i++) for (let j=0; j<numBalls; j++) interaction(i, j);
     if (!clicking) {
         if (infoScreen) clicked = true;
         else clicked = false;
         if (gravOn) gravClicked = true;
         else gravClicked = false;
+        if (obl) oblClicked = true;
+        else oblClicked = false;
+        if (bGrav) bGravClicked = true;
+        else bGravClicked = false;
     }
 
     if (infoScreen) {
         ctx.fillStyle = "rgb(100,100,100)";
-        ctx.fillRect(30, 30, 300, 150);
+        ctx.fillRect(30, 30, 300, 170);
         ctx.fillStyle = 'white';
         ctx.font ='20px Arial';
         ctx.fillText("Click around the screen", 50, 60);
@@ -138,16 +254,47 @@ function playing() {
         ctx.beginPath();
         ctx.rect(160, 75, 25, 25);
         ctx.stroke();
+
+        ctx.fillText("Oblique Collisions", 50, 130);
+        ctx.beginPath();
+        ctx.rect(190, 115, 25, 25);
+        ctx.stroke();
+
+        ctx.fillText("Intraball Gravity", 50, 170);
+        ctx.beginPath();
+        ctx.rect(170, 155, 25, 25);
+        ctx.stroke();
+
         if (gravOn) {
             ctx.fillStyle = 'red';
             ctx.fillRect(161,76,23,23);
         }
+        if (obl) {
+            ctx.fillStyle = 'red';
+            ctx.fillRect(191,116,23,23);
+        }
+        if (bGrav) {
+            ctx.fillStyle = 'red';
+            ctx.fillRect(171,156,23,23);
+        }
     }
 }
 function start() {
-    for (let i=0; i<150; i++) {
-        let col = `rgb(${rand(160,255)}, ${rand(160,255)}, ${rand(160,255)})`;
-        arr.push(new Particle(rand(30,window.innerWidth-50), rand(30,window.innerHeight-50), rand(-5, 6), rand(-5, 5), rand(3, 13), col));
+    numBalls++;
+    for (let i=0; i<numBalls; i++) {
+        let temp = [];
+        for (let j=0; j<numBalls; j++) {
+            temp.push(false);
+        }
+        colliding.push(temp);
     }
+    numBalls--;
+    for (let i=0; i<numBalls; i++) {
+        let col = `rgb(${rand(160,255)}, ${rand(160,255)}, ${rand(160,255)})`;
+        arr.push(new Particle(rand(30,window.innerWidth-50), rand(30,window.innerHeight-50), rand(-5, 6), rand(-5, 5), rand(3, 10), col));
+    }
+    numBalls++;
+    let col = `rgb(${rand(160,255)}, ${rand(160,255)}, ${rand(160,255)})`;
+    arr.push(new Particle(rand(30,window.innerWidth-50), rand(30,window.innerHeight-50), rand(-5, 6), rand(-5, 5), rand(150, 170), col));
     playing();
 }
